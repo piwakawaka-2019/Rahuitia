61b424bcba0b5cb42cf4c99012c6bc4bef1284bc
Nanz Nair <nanz89@gmail.com>
2019-05-10 10:05:44 +1200

    friday first commit

---
+++ client/actions/rahui.js
@@ -0,0 +1,24 @@
+
+
+
+// export function fetchAllRahui () {
+//     console.log
+//     return dispatch => { 
+//         gettAllRahui().then(allrahui => {
+//             dispatch(saveAllRahui(allrahui))
+//         })
+        
+//     }
+// }
+
+
+
+// export function saveAllRahui (allrahui) {
+//     return {
+//         type: 'SAVE_ALLRAHUI',
+//         rahui: allrahui
+//     }
+// }
+
+
+  
