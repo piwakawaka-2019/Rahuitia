3e6143a31230d0d39fbea4ea1a157974ae20691c
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-05-10 20:32:21 +1200

    About component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import Map from './Map'
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Map from "./Map";
+import About from "./About";
 
-export function App({auth}) {
+export function App({ auth }) {
   return (
-    <div>  
-    <Router>    
-      <div className="wrapper">
-      Rāhui 
-      </div>
-    </Router>
-    <Map/>
-    </div>  
-  )
+    <div>
+      <Router>
+        <div className="wrapper">Rāhui</div>
+      </Router>
+      {/* <Map/> */}
+      <About />
+    </div>
+  );
 }
 
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = ({ auth }) => {
   return {
     auth
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
