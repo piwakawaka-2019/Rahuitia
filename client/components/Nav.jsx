dba584253f65ee01b1bdfb458a3fc3569fd88324
blaineanthonywestern <blaine.a.western@gmail.com>
2019-05-11 10:11:44 +1200

    add navbar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,12 +4,12 @@ export class Nav extends Component {
   render() {
     return (
       <div className="navbar">
-        <div className="logo"></div>
-        <div className="index"></div>
-        <div className="about"></div>
-        <div className="login"></div>
-        <div className="explore"></div>
-        <div className="add"></div>
+        <div className="logo"><a href="">Rāhui</a></div>
+        <div className="index navButton"><a href="">INDEX</a></div>
+        <div className="about navButton"><a href="">ABOUT</a></div>
+        <div className="login navButton"><a href="">LOGIN</a></div>
+        <div className="explore navButton"><a href="">EXPLORE</a></div>
+        <div className="add navButton"><a href="">ADD A RĀHUI</a></div>
       </div>
     )
   }
