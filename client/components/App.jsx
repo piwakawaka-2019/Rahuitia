5870e3607099eea429a384a1a10230373b35ef6a
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-05-11 20:39:58 +1200

    about

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import Nav from './Nav'
-import Explore from './Explore'
-import AddRahuiMap from './NewMap'
+import Nav from "./Nav";
+import Explore from "./Explore";
+import AddRahuiMap from "./NewMap";
 
-import RahuiDetail from './RahuiDetail';
-
+import RahuiDetail from "./RahuiDetail";
 
 import About from "../components/about/About";
 import Team from "../components/about/Team";
@@ -17,31 +15,29 @@ import Resourses from "../components/about/Resourses";
 
 export function App({ auth }) {
   return (
-    <div>  
-    <Router>    
-      <Nav/>
-      <Route exact='true' path='/addrahui' component={AddRahuiMap}   />
-      <Route exact='true' path='/' component={Explore} />
-      <Route path='/rahui/:id' component={RahuiDetail} />
+    <div>
+      <Router>
+        <Nav />
+        <Route exact="true" path="/addrahui" component={AddRahuiMap} />
+        <Route exact="true" path="/" component={Explore} />
+        <Route path="/rahui/:id" component={RahuiDetail} />
 
-      <Route  path='/about/' component={About} />
-      <Route path='/about/team' component={Team} />
-      <Route path='/about/resourses' component={Resourses} />
+        <Route path="/about/" component={About} />
+        <Route path="/about/team" component={Team} />
+        <Route path="/about/resourses" component={Resourses} />
+      </Router>
+      {/* <Map/> */}
+      {/* <MapWithADrawingManager/> */}
+      {/* <Explore/> */}
 
-    </Router>
-    {/* <Map/> */}
-    {/* <MapWithADrawingManager/> */}
-    {/* <Explore/> */}
-
-    {/* <About /> */}
- 
-    </div>  
-  )
+      {/* <About /> */}
+    </div>
+  );
 }
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = ({ auth }) => {
   return {
     auth
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,17 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+// import About from "../components/about/About";
 
 export class Nav extends Component {
   render() {
     return (
       <div className="navbar">
-        <div className="logo"><a href="">Rāhui</a></div>
-        <div className="about navButton"><a href="">ABOUT</a></div>
-        <div className="login navButton"><a href="">LOGIN</a></div>
-        <div className="explore navButton"><a href="">EXPLORE</a></div>
-        <div className="add navButton"><a href="">ADD A RĀHUI</a></div>
+        <div className="logo">
+          <Link to="/">Rāhui</Link>
+        </div>
+        <div className="about navButton">
+          <Link to="/about/">ABOUT</Link>
+        </div>
+
+        <div className="login navButton">
+          <Link to="">LOGIN</Link>
+        </div>
+        <div className="explore navButton">
+          <Link to="">EXPLORE</Link>
+        </div>
+        <div className="add navButton">
+          <Link to="">ADD A RĀHUI</Link>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Nav
+export default Nav;
