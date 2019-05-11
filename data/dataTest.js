90df290dcfca8a29eb0f0194bdeaf42e4fb68b8b
Luke-Enoka <luke.enoka@gmail.com>
2019-05-11 12:33:42 +1200

    create Register component

---
+++ data/dataTest.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+      "Te Tai Tokerau": [
+        {
+          "Ngāi Takoto": [],
+          "Ngāti Kurī": [],
+          "Te Aupōuri": [],
+          "Ngāti Kahu": [
+            "Matakairiri / Pikaahu",
+            "Matarahurahu",
+            "Ngāi Tohianga",
+            "Ngāti Ruaiti",
+            "Ngāti Takiora / Ngāi Tauurutakaware",
+            "Ngāti Tara ki Parapara",
+            "Ngāti Tara ki Werowero",
+            "Ngāti Taranga",
+            "Ngāti Te Rūrunga / Te Paatu",
+            "Patu Kōraha",
+            "Te Paatu ki Pamāpūria",
+            "Te Paatu ki Pēria",
+            "Te Tahawai",
+            "Te Whānau Moana",
+            "Te Whānau Moana / Te Rorohuri"
+          ]
+        }
+      ]
+    },
+]
+
