4d4983480dde813f0b12a12dd894833cd6dccc0d
Nanz Nair <nanz89@gmail.com>
2019-05-16 10:06:59 +1200

    latest

--- client/components/RahuiDetailG/DetailReo.jsx
+++ client/components/RahuiDetailG/DetailReo.jsx
@@ -3,21 +3,27 @@ import { connect } from 'react-redux'
 import SplitterLayout from 'react-splitter-layout';
 import Tautoko from "../Tautoko"
 import NewMap from '../NewMap'
-
+import { getUserTokenInfo } from '../../utils/auth'
 
 
 class DetailReo extends React.Component {
   constructor(props) {
       super(props);
       this.state = {  }
+      this.userId = this.userId.bind(this)
   }
 
+
+
+    userId = () => {
+      console.log("user token:", getUserTokenInfo(), "this props:", this.props)
+    return getUserTokenInfo().user_id != undefined ? getUserTokenInfo().user_id : 0 } 
   handleclick=()=>{
     window.location = `/#/explore`; 
     }
 
-   render() 
-  { let {id, geo_ref, iwi_name, description, last_name, first_name, hapu_name, date_placed, date_lifted, korero, region, authoriser, contact, iwi, hapu} = this.props
+   render()
+  { let {id, geo_ref, iwi_name, description, last_name, first_name, hapu_name, date_placed, date_lifted, korero, region, authoriser, contact, iwi, hapu, user_id } = this.props
     return (
 
 <div>
@@ -28,7 +34,7 @@ class DetailReo extends React.Component {
 
     <div className="detailwrapper">
         <button style={{width:"15%" , marginBottom:"10px", padding:"0px"}} className="secondarybutton" onClick={this.handleclick.bind(this)}> Back to List </button>
-       
+        {/* <h1>{console.log("usertokeninfo:", getUserTokenInfo())}</h1> */}
         <h1> {description}</h1> 
         <Tautoko />
         <br/><b>Issued by </b><p> {hapu}   •  {iwi} •  {region}</p> 
@@ -46,6 +52,8 @@ class DetailReo extends React.Component {
         </div>
         <br></br>
         <hr></hr> 
+        {this.userId() == user_id ? <Link to={`/rahui/${id}/edit`}>Edit</Link> : "" }
+        <h1>{user_id}</h1>
         <div className="spaceme" />
         
    </div> 
