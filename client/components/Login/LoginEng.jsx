662f22234c51f2756374e3e330e695596b043655
Luke-Enoka <luke.enoka@gmail.com>
2019-05-29 09:22:52 +1200

    merge

--- client/components/Login/LoginEng.jsx
+++ client/components/Login/LoginEng.jsx
@@ -33,11 +33,9 @@ class LoginEng extends Component {
 
       <div className="login-wrapper">
 
-        <form
-          onSubmit={this.submit}
-          noValidate
-          className="login-wrapper" >
-          <h2 className="loginHeader" >Log in</h2>
+        <form onSubmit={this.submit} noValidate>
+
+          <h2>Log in</h2>
 
           <input className="loginInput" name="email" type="text" placeholder="email" noValidate onChange={this.handleChange} />
 
