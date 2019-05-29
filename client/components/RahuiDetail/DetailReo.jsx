6a77bd7e7099503c6ade62eb703423242539abbc
blaineanthonywestern <blaine.a.western@gmail.com>
2019-05-29 21:38:44 +1200

    add styling to detail

--- client/components/RahuiDetail/DetailReo.jsx
+++ client/components/RahuiDetail/DetailReo.jsx
@@ -3,8 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from "react-router-dom";
 import { getUserTokenInfo } from '../../utils/auth'
 import NewMap from '../NewMap'
-import SplitterLayout from 'react-splitter-layout';
-import Tautoko from "../Tautoko"
+import TautokoReo from "../Tautoko/TautokoReo"
 
 
 
@@ -32,38 +31,33 @@ class DetailReo extends React.Component {
     let { id, user_id, geo_ref, iwi_name, description, last_name, first_name, hapu_name, date_placed, date_lifted, korero, region, authoriser, contact, iwi, hapu } = this.props
 
     return (
-
-      <div>
-        <SplitterLayout primaryIndex={0}>
-          <div >
-            <NewMap color={"#2E86C1"} rahuiBounds={geo_ref} />
+      <React.Fragment>
+        <div className="mapBackground">
+          <NewMap color={"#2E86C1"} rahuiBounds={geo_ref} />
+        </div>
+        <div className="overlayNew">
+          <button style={{ width: "15%", marginBottom: "10px", padding: "0px" }} className="secondarybutton" onClick={this.handleclick.bind(this)}> hoki ki te Rārangi </button>
+          <h1> {description}</h1>
+          <TautokoReo />
+          <br /><b>I Whakaputaina e:</b><p>iwi:{iwi} •  hapu:{hapu}</p>
+          <br /><b>Te Ingoa o nga Kaituhi</b> <p> {authoriser}</p>
+          <br />
+          <div className="twocol">
+            <div ><b>Rā Kua Tuhia</b> <p> {date_placed}</p></div>
+            <div ><b>Rā Kua Piki</b> <p> {date_lifted}</p> </div>
           </div>
-            <div className="detailwrapper">
-              <button style={{ width: "15%", marginBottom: "10px", padding: "0px" }} className="secondarybutton" onClick={this.handleclick.bind(this)}> Back to List </button>
-              {/* <h1>{console.log("usertokeninfo:", getUserTokenInfo())}</h1> */}
-              <h1> {description}</h1>
-              <Tautoko />
-              <br /><b>Issued by </b><p> {hapu}   •  {iwi} •  {region}</p>
-              <br /><b>Authorised by</b> <p> {authoriser}</p>
-              <br />
-              <div className="twocol">
-                <div ><b>Date placed</b> <p> {date_placed}</p></div>
-                <div ><b>Date Lifted</b> <p> {date_lifted}</p> </div>
-              </div>
-              <br /><b>Korero</b> <p> {korero}</p>
-              <br /><br></br>
-              <div className="twocol">
-                <div><b>Submitted by</b> <p> {first_name} {last_name}</p></div>
-                <div><b>Contact</b> <p> {contact}</p></div>
-              </div>
-              <br></br>
-              <hr></hr>
-              <h3>{this.userId() == user_id ? <Link to={`/rahui/${id}/edit`}>Edit</Link> : ""}</h3>
-              <div className="spaceme" />
+          <br /><b>Korero</b> <p> {korero}</p>
+          <br /><br></br>
+          <div className="twocol detailbottom">
+            <div><b>Tukuna e</b> <p> {first_name} {last_name}</p></div>
+            <div><b>Whakapa Mai</b> <p> {contact}</p></div>
           </div>
-
-        </SplitterLayout>
-      </div>)
+          <br></br>
+          <h3>{this.userId() == user_id ? <Link to={`/rahui/${id}/edit`}>whakatika</Link> : ""}</h3>
+          <div className="spaceme" />
+        </div>
+      </React.Fragment>
+ )
   }
 }
 
