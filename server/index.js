d699934efb4a961990575103ffd36747fdaad008
Engie Matene <ematene@trademe.local>
2019-05-31 15:57:58 +1200

    update heroku variables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const envConfig = require('dotenv').config()
 if(envConfig.error) throw envConfig.error
 
+var environment = process.env.NODE_ENV || 'development'
+var config = require('../knexfile')[environment]
+var db = require('knex')(config)
+
 const server = require('./server')
 const PORT = process.env.PORT || 3000
 
