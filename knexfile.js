b3a2e437f86f87207d9f9b1602ca856245435436
Engie Matene <ematene@trademe.local>
2019-06-02 09:28:08 +1200

    fixed db connection to migrations

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
       client: 'pg',
       connection: process.env.DATABASE_URL + `?ssl=true`,
       migrations: {
-        directory: './db/migrations'
+        directory: './migrations'
       },
       useNullAsDefault: true
     }
