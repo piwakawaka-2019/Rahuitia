656ba7760d89936e1cec7802927f9f20ee783c3c
Engie Matene <ematene@trademe.local>
2019-06-02 09:39:02 +1200

    fixed seed data to use integers in date latest col

--- seeds/rahui.js
+++ seeds/rahui.js
@@ -17,7 +17,7 @@ exports.seed = function (knex, Promise) {
           geo_ref: JSON.stringify([{ lat: -35.16643533792861, lng: 174.3177917555065 }, { lat: -35.188043311071716, lng: 174.1965988233776 }, { lat: -35.23264419283272, lng: 174.2728164747448 }, { lat: -35.16643533792861, lng: 174.3177917555065 }
           ]),
           date_placed: '01-12-2010',
-          date_lifted: 'This Rāhui is still active',
+          date_lifted: '25-04-2019',
           authoriser: 'Robert Willoughby',
           contact: '09 403 7012',
           region: JSON.stringify(["Te Tai Tokerau"])
@@ -54,7 +54,7 @@ exports.seed = function (knex, Promise) {
             { lat: -35.55578112464781, lng: 174.5075801639855 }, { lat: -35.56157660076433, lng: 174.51556241801381 }, { lat: -35.55878365253887, lng: 174.5185664921105 }, { lat: -35.55396558810553, lng: 174.50989759257436 }, { lat: -35.55578112464781, lng: 174.5075801639855 }
           ]),
           date_placed: '19-04-2019',
-          date_lifted: 'This Rāhui is still active',
+          date_lifted: '25-04-2019',
           authoriser: 'Aperahama (Abe) Edwards',
           contact: 'Kensington blvd',
           region: JSON.stringify(["Te Tai Tokerau"])
