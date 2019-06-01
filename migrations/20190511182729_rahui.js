a5e841f9a8c7846f8dc25f8d56bc1d9d8274972d
Engie Matene <ematene@trademe.local>
2019-06-02 09:59:17 +1200

    changed all string col to text col

--- migrations/20190511182729_rahui.js
+++ migrations/20190511182729_rahui.js
@@ -2,17 +2,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("rahui", table => {
     table.increments("id").primary();
-    table.string("user_id");
-    table.string("region");
+    table.text("user_id");
+    table.text("region");
     table.text("iwi");
     table.text("hapu");
-    table.string("authoriser");
+    table.text("authoriser");
     table.text("description");
     table.text("korero");
-    table.string("geo_ref");
+    table.text("geo_ref");
     table.integer("date_placed");
     table.integer("date_lifted");
-    table.string("contact")
+    table.text("contact")
   });
 };
 
