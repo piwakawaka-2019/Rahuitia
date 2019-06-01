6a06ab9193084d0463877dad515e397071e06df4
Engie Matene <ematene@trademe.local>
2019-06-02 09:25:00 +1200

    fixing postgres connection

--- knexfile.js
+++ knexfile.js
@@ -25,22 +25,30 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'postgresql',
-    use_env_variable: "DATABASE_URL",
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-  
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+  // production: {
+  //   client: 'postgresql',
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL + `?ssl=true`,
+      migrations: {
+        directory: './db/migrations'
+      },
+      useNullAsDefault: true
     }
-  }
+    // use_env_variable: "DATABASE_URL",
+    // connection: {
+    //   database: 'my_db',
+    //   user:     'username',
+    //   password: 'password'
+    // },
+  
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  // }
 
 };
