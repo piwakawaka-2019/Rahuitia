6dca479ffad0daa2a4552b1d2449c5393492303c
blaineanthonywestern <blaine.a.western@gmail.com>
2019-06-06 18:57:08 +1200

    add toggle button to add rahui

--- client/components/Add/FormAndMap.jsx
+++ client/components/Add/FormAndMap.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom"
+import { fetchAllRahui } from "../../actions/rahui";
 import ReactMap from "../ReactMap";
 import AddRahuiForm from "./AddRahuiForm";
 
@@ -10,20 +11,45 @@ import { isAuthenticated } from "../../utils/auth"
 class Add extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            visibility: null,
+        }
+        this.handleClick = this.handleClick.bind(this)
     }
 
+    componentDidMount() {
+        this.props.dispatch(fetchAllRahui())
+        this.setState({
+            visibility: "list"
+        })
+    }
+
+    handleClick(e) {
+        e.preventDefault()
+        const { value } = e.target
+        this.setState({
+          visibility: value
+        });
+      }
 
     render() {
         return isAuthenticated() ? (
-            <div className="mapBackground">
-                <div >
+            <React.Fragment>
+                <div className="mapBackground" style={this.state.visibility == "map" ? {zIndex: 110} : {zIndex: 71}}>
                     <ReactMap />
                 </div>
-                <div className="overlayNew">
+                <div className="overlayNew" style={this.state.visibility == "list" ? {zIndex: 110} : {zIndex: 71}}>
                     <AddRahuiForm />
                 </div>
-            </div>
+                <div className="explore-buttons">
+                        <div className="explore-toggle-button">
+                        <button className={this.state.visibility == "list" ? "button-selected" : "button-deselected"} value="list" onClick={this.handleClick}>Details</button>
+                        </div>
+                        <div className="explore-toggle-button">
+                        <button className={this.state.visibility == "map" ? "button-selected" : "button-deselected"} value="map" onClick={this.handleClick}>Map</button>
+                        </div>
+                </div>        
+            </React.Fragment>
         ) : <Redirect to='/register' />
     }
 }
