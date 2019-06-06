798d5b0eee3c72cceb94090e8a173a659d543920
Luke-Enoka <luke.enoka@gmail.com>
2019-06-07 11:42:25 +1200

    about layout css

--- client/components/About/Team/TeamEng.jsx
+++ client/components/About/Team/TeamEng.jsx
@@ -5,14 +5,17 @@ class TeamEng extends Component {
     return (
       <div>
         <h1 className="aboutHeading2">Team</h1>
-            <p className="aboutBody">
-              Until recently, the prevailing view assumed lorem ipsum was born
-              as a nonsense text. “It's not Latin, though it looks like it, and
-              it actually says nothing,” Before & After magazine answered a
-              curious reader, “Its ‘words’ loosely approximate the frequency
-              with which letters occur in English, which is why at a glance it
-              looks pretty real.
-             </p> 
+        <div className="aboutBodyContainer">
+          <p className="aboutBody">
+            Until recently, the prevailing view assumed lorem ipsum was born
+            as a nonsense text. “It's not Latin, though it looks like it, and
+            it actually says nothing,” Before & After magazine answered a
+            curious reader, “Its ‘words’ loosely approximate the frequency
+            with which letters occur in English, which is why at a glance it
+            looks pretty real.
+             </p>
+        </div>
+        <div className="spaceme2"></div>
       </div>
     );
   }
